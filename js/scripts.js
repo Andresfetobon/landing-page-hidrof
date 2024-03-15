7e6560b66b8f25b9e56535e8614f730897af3bf6
Andresfetobon <andrestromp777@gmail.com>
2024-03-15 11:20:35 -0500

    Modificacion

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,23 @@
 // scroll librery init
-AOS.init(); 
-
+AOS.init();
 
 //navbar init
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, options);
-  console.log(instances)
+  console.log(instances);
 });
 
-
 // Carrusel Init
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
-   console.log(instances)
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, options);
+  console.log(instances);
+});
 
-  });
-
-  var options = {
-    indicator: true,
-    numVisible: 5,
-    padding: 15,
-  } 
+var options = {
+  indicator: true,
+  numVisible: 5,
+  padding: 15,
+};
